5243d97388316213654c65cf42c03f4848cfb744
muhammadsheraz <msherazjaved@gmail.com>
2024-03-02 18:00:37 +0500

    Added Content Security Policy to mitigate Cross Site Scripting (XSS) and data injection attacks

---
+++ app/Http/Middleware/ContentSecurityPolicy.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class ContentSecurityPolicy
+{
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+
+        $csp = "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self'; object-src 'none'";
+
+        $response->headers->set('Content-Security-Policy', $csp);
+
+        return $response;
+    }
+}
